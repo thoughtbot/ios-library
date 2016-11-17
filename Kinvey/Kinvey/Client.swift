6b335faf5c638462a2ea47947dbef1ef26237bb0
tejasranade <tejas@Tejass-MacBook-Pro.local>
2016-11-17 15:43:49 -0500

    Pubnub prototype

--- Kinvey/Kinvey/Client.swift
+++ Kinvey/Kinvey/Client.swift
@@ -8,12 +8,13 @@
 
 import Foundation
 import ObjectMapper
+import PubNub
 
 private let lockEncryptionKey = NSLock()
 
 /// This class provides a representation of a Kinvey environment holding App ID and App Secret. Please *never* use a Master Secret in a client application.
 @objc(__KNVClient)
-open class Client: NSObject, NSCoding, Credential {
+open class Client: NSObject, NSCoding, Credential, PNObjectEventListener {
 
     /// Shared client instance for simplicity. Use this instance if *you don't need* to handle with multiple Kinvey environments.
     open static let sharedClient = Client()
@@ -227,6 +228,15 @@ open class Client: NSObject, NSCoding, Credential {
                 activeUser = user
             }
         }
+        
+        // Initialize and configure PubNub client instance
+        let configuration = PNConfiguration(publishKey: "pub-c-a43d9483-0340-45ea-83c5-6eb9badc0127", subscribeKey: "sub-c-13aebfae-ac4f-11e6-8319-02ee2ddab7fe")
+        self.client = PubNub.clientWithConfiguration(configuration)
+        self.client.addListener(self)
+        
+        // Subscribe to demo channel with presence observation
+        self.client.subscribeToChannels(["my_channel"], withPresence: true)
+
     }
     
     /// Autorization header used for calls that don't requires a logged `User`.
@@ -277,4 +287,129 @@ open class Client: NSObject, NSCoding, Credential {
         aCoder.encode(apiHostName, forKey: "apiHostName")
         aCoder.encode(authHostName, forKey: "authHostName")
     }
+    
+    
+    
+    // Stores reference on PubNub client to make sure what it won't be released.
+    var client: PubNub!
+    
+    // Handle new message from one of channels on which client has been subscribed.
+    public func client(_ client: PubNub, didReceiveMessage message: PNMessageResult) {
+        
+        // Handle new message stored in message.data.message
+        if message.data.channel != message.data.subscription {
+            
+            // Message has been received on channel group stored in message.data.subscription.
+        }
+        else {
+            
+            // Message has been received on channel stored in message.data.channel.
+        }
+        
+        print("Received message: \(message.data.message) on channel \(message.data.channel) " +
+            "at \(message.data.timetoken)")
+    }
+    
+    // New presence event handling.
+    public func client(_ client: PubNub, didReceivePresenceEvent event: PNPresenceEventResult) {
+        
+        // Handle presence event event.data.presenceEvent (one of: join, leave, timeout, state-change).
+        if event.data.channel != event.data.subscription {
+            
+            // Presence event has been received on channel group stored in event.data.subscription.
+        }
+        else {
+            
+            // Presence event has been received on channel stored in event.data.channel.
+        }
+        
+        if event.data.presenceEvent != "state-change" {
+            
+            print("\(event.data.presence.uuid) \"\(event.data.presenceEvent)'ed\"\n" +
+                "at: \(event.data.presence.timetoken) on \(event.data.channel) " +
+                "(Occupancy: \(event.data.presence.occupancy))");
+        }
+        else {
+            
+            print("\(event.data.presence.uuid) changed state at: " +
+                "\(event.data.presence.timetoken) on \(event.data.channel) to:\n" +
+                "\(event.data.presence.state)");
+        }
+    }
+    
+    // Handle subscription status change.
+    public func client(_ client: PubNub, didReceive status: PNStatus) {
+        
+        if status.operation == .subscribeOperation {
+            
+            // Check whether received information about successful subscription or restore.
+            if status.category == .PNConnectedCategory || status.category == .PNReconnectedCategory {
+                
+                let subscribeStatus: PNSubscribeStatus = status as! PNSubscribeStatus
+                if subscribeStatus.category == .PNConnectedCategory {
+                    
+                    // This is expected for a subscribe, this means there is no error or issue whatsoever.
+                    
+                    // Select last object from list of channels and send message to it.
+                    let targetChannel = client.channels().last!
+                    client.publish("Hello from the PubNub Swift SDK", toChannel: targetChannel,
+                                   compressed: false, withCompletion: { (publishStatus) -> Void in
+                                    
+                                    if !publishStatus.isError {
+                                        
+                                        // Message successfully published to specified channel.
+                                    }
+                                    else {
+                                        
+                                        /**
+                                         Handle message publish error. Check 'category' property to find out
+                                         possible reason because of which request did fail.
+                                         Review 'errorData' property (which has PNErrorData data type) of status
+                                         object to get additional information about issue.
+                                         
+                                         Request can be resent using: publishStatus.retry()
+                                         */
+                                    }
+                    })
+                }
+                else {
+                    
+                    /**
+                     This usually occurs if subscribe temporarily fails but reconnects. This means there was
+                     an error but there is no longer any issue.
+                     */
+                }
+            }
+            else if status.category == .PNUnexpectedDisconnectCategory {
+                
+                /**
+                 This is usually an issue with the internet connection, this is an error, handle
+                 appropriately retry will be called automatically.
+                 */
+            }
+                // Looks like some kind of issues happened while client tried to subscribe or disconnected from
+                // network.
+            else {
+                
+                let errorStatus: PNErrorStatus = status as! PNErrorStatus
+                if errorStatus.category == .PNAccessDeniedCategory {
+                    
+                    /**
+                     This means that PAM does allow this client to subscribe to this channel and channel group
+                     configuration. This is another explicit error.
+                     */
+                }
+                else {
+                    
+                    /**
+                     More errors can be directly specified by creating explicit cases for other error categories
+                     of `PNStatusCategory` such as: `PNDecryptionErrorCategory`,
+                     `PNMalformedFilterExpressionCategory`, `PNMalformedResponseCategory`, `PNTimeoutCategory`
+                     or `PNNetworkIssuesCategory`
+                     */
+                }
+            }
+        }
+    }
+
 }
